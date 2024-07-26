582d0474bca318c12ae8cbbaa535268b8074960d
igornb <igornb@ya.ru>
2024-07-26 21:47:39 +0700

    fix

--- main/02-odd-and-even/test.py
+++ main/02-odd-and-even/test.py
@@ -7,24 +7,21 @@ from utils import testUtil
 class TestCase(unittest.TestCase):
     def test_case1(self):
         value = testUtil.file_test(
-            """6
--1 -1 -9 -7 3 -6
-2""",
+            """1 2 -3
+""",
             task.main,
         )
         self.assertEqual(
             value.strip(),
-            """-1 3""",
+            """FAIL""",
         )
 
     def test_case2(self):
         value = testUtil.file_test(
-            """8
-6 2 8 -3 1 1 6 10
-100""",
+            f"""{10**9} {-10**9} 2""",
             task.main,
         )
-        self.assertEqual(value.strip(), """None""")
+        self.assertEqual(value.strip(), """WIN""")
 
 
 if __name__ == "__main__":
