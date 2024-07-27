1c6bb10a7389c4c7045d7268f717dbf123f8185e
igornb <igornb@ya.ru>
2024-07-27 21:17:49 +0700

    fix

--- intro/03-two-chips/task.py
+++ intro/03-two-chips/task.py
@@ -1,8 +1,8 @@
 def main():
-    with open("input.txt") as inp, open("output.txt", "w") as of:
-        n = int(inp.readline())
-        l = list(map(int, inp.readline().split()))
-        k = int(inp.readline())
+    with open("input.txt") as input_file, open("output.txt", "w") as output_file:
+        n = int(input_file.readline())
+        l = list(map(int, input_file.readline().split()))
+        k = int(input_file.readline())
         res = None
         for i in range(n - 1):
             for j in range(i + 1, n):
@@ -10,7 +10,7 @@ def main():
                 if a + b == k:
                     res = (a, b)
                     break
-        print(*res if res else (res,), file=of)
+        print(*res if res else (res,), file=output_file)
 
 
 if __name__ == "__main__":
