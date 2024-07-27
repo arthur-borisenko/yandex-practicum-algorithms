7f3168e07e431c92835a27ed93eb30b982bc8fdd
igornb <igornb@ya.ru>
2024-07-27 22:54:39 +0700

    fix

---
+++ main/06-palindrom/test.py
@@ -0,0 +1,34 @@
+import task
+import unittest
+
+from utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """A man, a plan, a canal: Panama
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """True
+""",
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """zo
+            """,
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """False
+""",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
