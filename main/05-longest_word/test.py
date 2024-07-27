972c47e72500fa797265e9658a773580188f0d3d
igornb <igornb@ya.ru>
2024-07-27 22:05:40 +0700

    fix

---
+++ main/05-longest_word/test.py
@@ -0,0 +1,35 @@
+import task
+import unittest
+
+from utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """19
+i love segment tree
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """segment
+7
+""",
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """21
+frog jumps from river
+""",
+            task.main,
+        )
+        self.assertEqual(value, """jumps
+5
+""")
+
+
+if __name__ == "__main__":
+    unittest.main()
