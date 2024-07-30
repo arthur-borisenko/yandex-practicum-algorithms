ad801603b6fec711e4ead1794e69125edf4b3782
igornb <igornb@ya.ru>
2024-07-30 22:16:35 +0700

    fix

--- main/08-binary-system/test.py
+++ main/08-binary-system/test.py
@@ -7,25 +7,27 @@ from utils import testUtil
 class TestCase(unittest.TestCase):
     def test_case1(self):
         value = testUtil.file_test(
-            """5
+            """1010
+1011
 """,
             task.main,
         )
         self.assertEqual(
             value,
-            """101
+            """10101
 """,
         )
 
     def test_case2(self):
         value = testUtil.file_test(
-            """14
+            """1
+1
             """,
             task.main,
         )
         self.assertEqual(
             value,
-            """1110
+            """10
 """,
         )
 
