fd3b168d0279e4c410535b4bf36294c2316e5d82
igornb <igornb@ya.ru>
2024-08-01 22:41:17 +0700

    fix

--- main/10-factorization/test.py
+++ main/10-factorization/test.py
@@ -7,7 +7,7 @@ from utils import testUtil
 
 
 def is_simple(n: int):
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
