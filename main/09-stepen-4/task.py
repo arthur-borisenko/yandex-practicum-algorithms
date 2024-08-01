f18fd1f1874a800e486f12515471ec489244508b
igornb <igornb@ya.ru>
2024-08-01 22:46:43 +0700

    fix

--- main/09-stepen-4/task.py
+++ main/09-stepen-4/task.py
@@ -3,18 +3,18 @@ MAX_NUMBER = 10000
 
 def main():
     """
-    CPU - O(n)
-    RAM - o(n)
+    CPU - O(log4(n))
+    RAM - O(1)
     :return: Void
     """
     with open("input.txt") as inp, open("output.txt", "w") as outp:
         n = int(inp.readline())
-        current_power = 0
         res = False
-        while 4**current_power < MAX_NUMBER:
-            if 4**current_power == n:
+        next_power = 1
+        while next_power < MAX_NUMBER:
+            if next_power == n:
                 res = True
-            current_power += 1
+            next_power *= 4
         print(str(res), file=outp)
 
 
