8ff2ea6325d255b817f2ed2f885242d4b2e2fe61
igornb <igornb@ya.ru>
2024-08-12 21:15:48 +0700

    temp

--- main/19-vse-naoborot/task.py
+++ main/19-vse-naoborot/task.py
@@ -1,8 +1,11 @@
-class DoubleConnectedNode:
-    def __init__(self, value, next=None, prev=None):
-        self.value = value
-        self.next = next
-        self.prev = prev
+LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+
+if LOCAL:
+    class DoubleConnectedNode:
+        def __init__(self, value, next=None, prev=None):
+            self.value = value
+            self.next = next
+            self.prev = prev
 
 
 class TwoLinkedList:
