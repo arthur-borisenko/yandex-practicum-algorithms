2f01bcb45c1c53bdd506c65891e3305121818fd3
igornb <igornb@ya.ru>
2024-08-19 23:06:37 +0700

    add recursive solution

---
+++ main/30.1-combinations-recursion/test.py
@@ -0,0 +1,46 @@
+import task
+import unittest
+
+from utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """92
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """wa wb wc xa xb xc ya yb yc za zb zc
+""",
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """23
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """ad ae af bd be bf cd ce cf
+""",
+        )
+
+    def test_case3(self):
+        value = testUtil.file_test(
+            """2
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """a b c
+""",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
