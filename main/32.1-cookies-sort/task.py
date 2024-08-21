59607841c7583a48e924b8ef92be5a4da138f1c4
igornb <igornb@ya.ru>
2024-08-21 21:56:01 +0700

    add recursive solution

--- main/32.1-cookies-sort/task.py
+++ main/32.1-cookies-sort/task.py
@@ -1,14 +1,19 @@
-def count_sort_o_max_min(arr):
-    """CPU - O(n+max(arr)-min(arr))
-    RAM - O(n)"""
-    counts = {}
+import array
+
+
+def count_sort_o_max_min(arr, reverse=False):
+    """CPU - O(n+max(arr))
+    RAM - O(n+max(arr))"""
+    counts = array.array("q", [0] * (max(arr) + 1))
     for el in arr:
-        counts[el] = counts.get(el, 0) + 1
-    res = []
+        counts[el] = counts[el] + 1
+    res = array.array("q", [0] * len(arr))
+    index = 0
     for i in range(min(arr), max(arr) + 1):
-        for j in range(counts.get(i, 0)):
-            res.append(i)
-    return res
+        for j in range(counts[i]):
+            res[index] = i
+            index += 1
+    return array.array("q", reversed(res)) if reverse else res
 
 
 def solve(children, cookies):
@@ -27,6 +32,8 @@ def solve(children, cookies):
 
 
 def main():
+    """CPU - O(n+m+max(children)+max(cookies)
+    RAM - O(n+m+max(children)+max(cookies)"""
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         n = int(inp.readline())
         children = list(map(int, inp.readline().strip().split()))
