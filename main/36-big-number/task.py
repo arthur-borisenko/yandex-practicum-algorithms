f56118a1c6d58f6aa598b7330883f2cb087aa5db
igornb <igornb@ya.ru>
2024-08-21 23:52:22 +0700

    add recursive solution

--- main/36-big-number/task.py
+++ main/36-big-number/task.py
@@ -1,22 +1,24 @@
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i
-        while j > 0 and arr[j - 1] > arr[j]:
-            arr[j] = arr[j - 1]
-            j -= 1
-        arr[j] = key
+def bubble_sort(
+    arr, key=lambda x: x, less_comparator=lambda x, y: x < y, reverse=False
+):
+    new_arr = list(map(key, arr))
+    for i in range(len(new_arr) - 1):
+        for j in range(len(new_arr) - i - 1):
+            if less_comparator(new_arr[j], new_arr[j + 1]):
+                new_arr[j], new_arr[j + 1] = new_arr[j + 1], new_arr[j]
+    return list(reversed(new_arr)) if reverse else new_arr
 
 
 def main():
-    """CPU - O(n)
-    RAM - O(n)"""
+    """CPU - O(n**2)
+    RAM - O(1)"""
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         n = int(inp.readline())
-        inp_data = list(
-            map(lambda x: int, filter(lambda x: x.isdigit(), inp.readline().split()))
+        inp_data = inp.readline().split()
+        print(
+            "".join(bubble_sort(inp_data)),
+            file=outp,
         )
-        insertion_sort(inp_data)
 
 
 if __name__ == "__main__":
--- main/36-big-number/test.py
+++ main/36-big-number/test.py
@@ -7,56 +7,40 @@ from utils import testUtil
 class TestCase(unittest.TestCase):
     def test_case1(self):
         value = testUtil.file_test(
-            """10
-put -34
-put -23
-get
-size
-get
-size
-get
-get
-put 80
-size
+            """3
+15 56 2
 """,
             task.main,
         )
         self.assertEqual(
             value,
-            """-34
-1
--23
-0
-error
-error
-1
+            """56215
 """,
         )
 
     def test_case2(self):
         value = testUtil.file_test(
-            """9
-get
-size
-put 74
-get
-size
-put 90
-size
-size
-size
+            """3
+1 783 2
 """,
             task.main,
         )
         self.assertEqual(
             value,
-            """error
-0
-74
-0
-1
-1
-1
+            """78321
+""",
+        )
+
+    def test_case3(self):
+        value = testUtil.file_test(
+            """5
+2 4 5 2 10
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """542210
 """,
         )
 
