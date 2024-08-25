a06a3e1c3a7f626dfe967d97920e72278413c6d1
Art <artour.borisenko@ya.ru>
2024-08-25 19:11:36 +0300

    draft

--- main/33.1-pokupka-domov-sort/task.py
+++ main/33.1-pokupka-domov-sort/task.py
@@ -1,25 +1,38 @@
 import array
+from typing import Sequence, Iterable
 
 
-def count_sort_o_max_min(arr, reverse=False):
+def count_sort_to_orig_arr_iterator(cnt_arr, shift=0):
+    """CPU - O(n)
+    RAM - O(1)
+    May not be used outside count sort
+    :param cnt_arr: counts array
+    :param shift: value shift of orig array
+    :return: result iterator"""
+    for i, cnt in enumerate(cnt_arr):
+        for _ in range(cnt):
+            yield i + shift
+
+
+def count_sort(seq: Sequence[int]) -> Iterable[int]:
     """CPU - O(n+max(arr))
-    RAM - O(n+max(arr))"""
-    counts = array.array("q", [0] * (max(arr) + 1))
-    for el in arr:
-        counts[el] = counts[el] + 1
-    res = array.array("q", [0] * len(arr))
-    index = 0
-    for i in range(min(arr), max(arr) + 1):
-        for j in range(counts[i]):
-            res[index] = i
-            index += 1
-    return array.array("q", reversed(res)) if reverse else res
+    RAM - O(max(arr) - min(arr))
+    sorts input sequence of positive integers using "count sort"
+    :param seq: array to be sorted
+    :return: iterator of sorted array"""
+    shift = min(seq)
+    counts = array.array("q", [0] * (max(seq) - shift + 1))
+    for el in seq:
+        counts[el - shift] += 1
+    return count_sort_to_orig_arr_iterator(counts, shift)
 
 
 def solve(homes, money):
+    """CPU - O(n+max(arr)-min(arr))
+    RAM - O(max(arr) - min(arr))"""
     res = 0
     current_money = money
-    sorted_homes = count_sort_o_max_min(homes)
+    sorted_homes = count_sort(homes)
     for home in sorted_homes:
         if current_money >= home:
             current_money -= home
