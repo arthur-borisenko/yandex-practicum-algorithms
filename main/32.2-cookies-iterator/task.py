9542f10d166633d3e4a875d5c2072bb118f31369
Art <artour.borisenko@ya.ru>
2024-08-25 20:03:18 +0300

    draft

--- main/32.2-cookies-iterator/task.py
+++ main/32.2-cookies-iterator/task.py
@@ -1,4 +1,5 @@
 import array
+from typing import Sequence, Iterable
 
 
 def count_sort_to_orig_arr_iterator(cnt_arr, shift=0):
@@ -6,27 +7,29 @@ def count_sort_to_orig_arr_iterator(cnt_arr, shift=0):
     RAM - O(1)
     May not be used outside count sort
     :param cnt_arr: counts array
-    :param shift: minimal element of orig array
-    :return: result iterator"""
+    :param shift: value shift of orig array
+    :return: iterator"""
     for i, cnt in enumerate(cnt_arr):
         for _ in range(cnt):
             yield i + shift
 
 
-def count_sort(arr):
+def count_sort(seq: Sequence[int]) -> Iterable[int]:
     """CPU - O(n+max(arr))
-    RAM - O(n+max(arr))
-    does not change source array
-    :param arr: array to be sorted
-    :return: result iterator"""
-    shift = min(arr)
-    counts = array.array("q", [0] * (max(arr) - shift + 1))
-    for el in arr:
+    RAM - O(max(arr) - min(arr))
+    sorts input sequence of positive integers using "count sort"
+    :param seq: array to be sorted
+    :return: iterator of sorted array"""
+    shift = min(seq)
+    counts = array.array("q", [0] * (max(seq) - shift + 1))
+    for el in seq:
         counts[el - shift] += 1
     return count_sort_to_orig_arr_iterator(counts, shift)
 
 
 def solve(children, cookies):
+    """CPU - O(n+m+max(children)+max(cookies))
+    RAM - O(n+max(children)+max(cookies))"""
     res = 0
     sorted_children, sorted_cookies = array.array(
         "q", count_sort(children)
