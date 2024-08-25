a2df1e58e9836705ae50a9d9331f7a555b5e1b0f
Art <artour.borisenko@ya.ru>
2024-08-25 23:21:36 +0300

    draft

--- main/36-big-number/task.py
+++ main/36-big-number/task.py
@@ -1,17 +1,27 @@
 import functools
 
+
+def compare(a, b):
+    if a > b:
+        return 1
+    if a < b:
+        return -1
+    else:
+        return 0
+
+
 def cmp(x, y):
-    ,
+    return compare(x + y, y + x)
+
+
 def main():
     """CPU - O(n**2)
     RAM - O(1)"""
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         n = int(inp.readline())
         inp_data = inp.readline().split()
-        print(
-            "".join(bubble_sort(inp_data)),
-            file=outp,
-        )
+        res = sorted(inp_data, key=functools.cmp_to_key(cmp), reverse=True)
+        print(*res, sep="", file=outp)
 
 
 if __name__ == "__main__":
