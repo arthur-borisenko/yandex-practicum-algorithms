2ef6aabd942cd8589b8196601ea5bfa846d19617
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2024-08-25 23:11:13 +0300

    Update task.py

--- main/35-garderob/task.py
+++ main/35-garderob/task.py
@@ -11,8 +11,7 @@ def count_sort_to_orig_arr_iterator(cnt_arr, shift=0):
 def count_sort(seq: Sequence[int]) -> Iterable[int]:
     """CPU - O(n + max(arr) - min(arr))
     RAM - O(max(arr) - min(arr))
-    sorts input sequence of non-negative integers using "count sort"
-    :param seq: array of non-negative integers(including 0) to be sorted
+    :param seq: array of non-negative integers to be sorted
     :return: iterator of sorted array"""
     shift = min(seq)
     counts = array.array("q", [0] * (max(seq) - shift + 1))
