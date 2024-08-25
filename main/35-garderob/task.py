515c43a620ac6bc1fb61c99805aa9686e9eae817
Art <artour.borisenko@ya.ru>
2024-08-25 23:08:18 +0300

    draft

--- main/35-garderob/task.py
+++ main/35-garderob/task.py
@@ -11,8 +11,8 @@ def count_sort_to_orig_arr_iterator(cnt_arr, shift=0):
 def count_sort(seq: Sequence[int]) -> Iterable[int]:
     """CPU - O(n + max(arr) - min(arr))
     RAM - O(max(arr) - min(arr))
-    sorts input sequence of positive integers using "count sort"
-    :param seq: array of positive integers(including 0) to be sorted
+    sorts input sequence of non-negative integers using "count sort"
+    :param seq: array of non-negative integers(including 0) to be sorted
     :return: iterator of sorted array"""
     shift = min(seq)
     counts = array.array("q", [0] * (max(seq) - shift + 1))
@@ -25,7 +25,7 @@ def main():
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         n = int(inp.readline())
         items = list(map(int, inp.readline().strip().split()))
-        print(*count_sort_o_max_min(items), file=outp)
+        print(*count_sort(items), file=outp)
 
 
 if __name__ == "__main__":
