56aa476c76fc6f4cee7195c616fe21b2c8e6e97d
Art <artour.borisenko@ya.ru>
2024-08-25 23:23:47 +0300

    draft

--- main/29-skobki/task.py
+++ main/29-skobki/task.py
@@ -1,17 +1,19 @@
-def gen_brackets(n, prefix, opening, closing, output_file):
-    if (n - 1) == 0:
-        if opening == closing:
+def gen_brackets(n, prefix, bracket, non_closed, output_file):
+    prefix += bracket
+    non_closed = non_closed + 1 if bracket == "(" else non_closed - 1
+    if n - 1 == 0:
+        if non_closed == 0:
             print(prefix, file=output_file)
     else:
-        gen_brackets(n - 1, prefix + "(", opening + 1, closing, output_file)
-        if opening >= closing + 1:
-            gen_brackets(n - 1, prefix + ")", opening, closing + 1, output_file)
+        gen_brackets(n - 1, prefix, "(", non_closed, output_file)
+        if non_closed > 0:
+            gen_brackets(n - 1, prefix, ")", non_closed, output_file)
 
 
 def main():
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         total = int(inp.readline()) * 2
-        gen_brackets(total, "(", 1, 0, outp)
+        gen_brackets(total, "", "(", 0, outp)
 
 
 if __name__ == "__main__":
