8a321dd88f5e0efbac1510fac7d1d6746ec1054c
Art <artour.borisenko@ya.ru>
2024-08-25 23:16:05 +0300

    draft

--- main/36-big-number/task.py
+++ main/36-big-number/task.py
@@ -1,14 +1,7 @@
-def bubble_sort(
-    arr, key=lambda x: x, less_comparator=lambda x, y: x < y, reverse=False
-):
-    new_arr = list(map(key, arr))
-    for i in range(len(new_arr) - 1):
-        for j in range(len(new_arr) - i - 1):
-            if less_comparator(new_arr[j], new_arr[j + 1]):
-                new_arr[j], new_arr[j + 1] = new_arr[j + 1], new_arr[j]
-    return list(reversed(new_arr)) if reverse else new_arr
+import functools
 
-
+def cmp(x, y):
+    ,
 def main():
     """CPU - O(n**2)
     RAM - O(1)"""
