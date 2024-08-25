fe8721d38489650a82dd6b52d82125417dca4ea3
Art <artour.borisenko@ya.ru>
2024-08-25 16:37:48 +0300

    draft

--- main/37-conferences/task.py
+++ main/37-conferences/task.py
@@ -1,41 +1,45 @@
-def bubble_sort_k_biggest_items(arr, k, key=lambda x: x):
-    new_arr = list(arr)
-    for i in range(k):
-        for j in range(len(new_arr) - i - 1):
-            if key(new_arr[j]) > key(new_arr[j + 1]):
-                new_arr[j], new_arr[j + 1] = new_arr[j + 1], new_arr[j]
-        pass
-    new_arr = new_arr[-k:]
-    return list(reversed(new_arr))
+from array import array
+from typing import Any
 
 
-def _count(arr):
-    """CPU - O(n)
-    RAM - O(n)"""
-    counts = {}
-    for el in arr:
-        counts[el] = counts.get(el, 0) + 1
-    return counts
+# asc sort. non existent (e.g. []) elements not excluded
+def count_sort_obj(arr: list[Any]) -> list[list[Any]]:
+    min_val = min(arr)  # TODO>>may be use less memory (min to max)
+    result: list[list[int]] = [[] for _ in range((max(arr) + 1))]
+    for i, val in enumerate(arr):
+        result[val].append(i)
+    return result
+
+
+# asc sort. non existent (e.g. 0) elements not excluded
+def count_sort(arr: array[int]) -> array[int]:
+    min_val = min(arr)  # TODO>>may be use less memory (min to max)
+    result = array("q", [0] * (max(arr) + 1))
+    for val in arr:
+        result[val] += 1
+    return result
 
 
 def main():
-    """CPU - O(nk)
-    RAM - O(n)"""
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         n = int(inp.readline())
-        arr = map(int, inp.readline().split())
+        stud_univer_ids = array("q", map(int, inp.readline().split()))
         k = int(inp.readline())
-        print(
-            " ".join(
-                map(
-                    lambda x: str(x[0]),
-                    bubble_sort_k_biggest_items(
-                        _count(arr).items(), k, key=lambda x: (x[1], -x[0])
-                    ),
-                )
-            ),
-            file=outp,
-        )
+        stud_cnt_per_univer_id = count_sort(stud_univer_ids)
+        univer_ids_per_stud_cnt = count_sort_obj(stud_cnt_per_univer_id)
+        univer_ids_desc = list(
+            reversed(univer_ids_per_stud_cnt[1:])
+        )  # skip univers with 0 stud, then reverse - populars first
+        print_output(k, univer_ids_desc, outp)
+
+
+def print_output(k, arr: list[list[int]], outp):
+    for i, sub_arr in enumerate(arr):
+        for j, val in enumerate(sub_arr):
+            print(val, end=" ", file=outp)
+            k -= 1
+            if k == 0:
+                return
 
 
 if __name__ == "__main__":
