dfa04a0dde7d421a8ca81a3677a82b7a49229067
Art <artour.borisenko@ya.ru>
2024-08-26 20:34:59 +0300

    re-structured

--- common/sorting/count_sort/count_sort/task.py
+++ common/sorting/count_sort/count_sort/task.py
@@ -2,14 +2,11 @@ import array
 from typing import Sequence, Iterable
 
 
-def count_sort_to_orig_arr_iterator(cnt_arr, shift=0, reverse=False):
-    """CPU - O(n)
+def count_sort_to_orig_arr_iterator(cnt_arr, shift=0, reverse=False) -> Iterable:
+    """CPU - O(1) to init, O(1) per iteration
     RAM - O(1)
     Must not be used outside count sort
-    :param cnt_arr: counts array
-    :param shift: value shift of orig array
-    :param reverse: reverse order of result
-    :return: iterator"""
+    """
     _range = range(len(cnt_arr)) if not reverse else reversed(range(len(cnt_arr)))
     for i in _range:
         cnt = cnt_arr[i]
@@ -20,10 +17,9 @@ def count_sort_to_orig_arr_iterator(cnt_arr, shift=0, reverse=False):
 def count_sort(seq: Sequence[int], reverse=False) -> Iterable[int]:
     """CPU - O(n+max(arr))
     RAM - O(max(arr) - min(arr))
-    Don't change input data structure
-    sorts input sequence of positive integers using "count sort"
-    :param seq: array to be sorted
-    :return: iterator of sorted array"""
+    Doesn't change input data structure
+    Input sequence must contain only non-negative integers
+    """
     shift = min(seq, default=0)  # , default=0)
     counts = array.array("q", [0] * (max(seq, default=0) - shift + 1))
     for el in seq:
--- common/sorting/count_sort/count_sort_inplace/task.py
+++ common/sorting/count_sort/count_sort_inplace/task.py
@@ -5,7 +5,7 @@ from typing import MutableSequence
 def count_sort(arr: MutableSequence[int], reverse=False) -> None:
     """CPU - O(n+max(arr))
     RAM - O(max(arr))
-    sort input array of ints (non-negative) with "count sort"
+    Input sequence must contain only non-negative integers
     """
     counts = array.array("q", [0] * (max(arr, default=0) + 1))
     for val in arr:
