904f4b1d5a0cc00e559cb524968e616a372307d9
Art <artour.borisenko@ya.ru>
2024-08-26 20:40:33 +0300

    re-structured

--- common/sorting/count_sort/count_sort/task.py
+++ common/sorting/count_sort/count_sort/task.py
@@ -2,18 +2,6 @@ import array
 from typing import Sequence, Iterable
 
 
-def count_sort_to_orig_arr_iterator(cnt_arr, shift=0, reverse=False) -> Iterable:
-    """CPU - O(1) to init, O(1) per iteration
-    RAM - O(1)
-    Must not be used outside count sort
-    """
-    _range = range(len(cnt_arr)) if not reverse else reversed(range(len(cnt_arr)))
-    for i in _range:
-        cnt = cnt_arr[i]
-        for _ in range(cnt):
-            yield i + shift
-
-
 def count_sort(seq: Sequence[int], reverse=False) -> Iterable[int]:
     """CPU - O(n+max(arr))
     RAM - O(max(arr) - min(arr))
@@ -24,4 +12,8 @@ def count_sort(seq: Sequence[int], reverse=False) -> Iterable[int]:
     counts = array.array("q", [0] * (max(seq, default=0) - shift + 1))
     for el in seq:
         counts[el - shift] += 1
-    return count_sort_to_orig_arr_iterator(counts, shift, reverse)
+    _range = range(len(counts)) if not reverse else reversed(range(len(counts)))
+    for i in _range:
+        cnt = counts[i]
+        for _ in range(cnt):
+            yield i + shift
