982fb4532b0d8bdfe85f62c3b93801a3f428ccaa
Art <artour.borisenko@ya.ru>
2024-09-19 15:07:45 +0300

    re-structured

---
+++ main/42-klumbi/task.py
@@ -0,0 +1,40 @@
+from array import array
+
+
+def main():
+    """CPU - O(n log(n))
+    RAM - O(n)"""
+    with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
+        print_output(solve(inp), file=outp)
+
+
+def print_output(out, file):
+    for line in out:
+        print(*line, file=file)
+
+
+def solve(inp):
+    """CPU - O(n log(n))
+    RAM - O(n)"""
+    n = int(inp.readline())
+    max_end = float("-inf")
+    max_end_pair = -1
+    res = []
+    res_pair_i = 0
+    lns = inp.readlines()
+    sorted_lns = sorted(map(lambda l: list(map(int, l.split())), lns))
+    for line in sorted_lns:
+        if line[0] > max_end:
+            res.append(line)
+            max_end = line[1]
+            max_end_pair = res_pair_i
+        else:
+            if line[1] > max_end:
+                res[max_end_pair][1] = line[1]
+                max_end = line[1]
+        res_pair_i += 1
+    return res
+
+
+if __name__ == "__main__":
+    main()
