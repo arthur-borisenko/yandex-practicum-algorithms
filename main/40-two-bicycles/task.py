ec26b247a42da05321fb84b5c4762f4b9c8e1aea
Art <artour.borisenko@ya.ru>
2024-10-30 12:03:02 +0300

    simplified alorythm

--- main/40-two-bicycles/task.py
+++ main/40-two-bicycles/task.py
@@ -3,22 +3,12 @@ def binary_nearest_search(seq, target, key=lambda x: x):
     end = len(seq) - 1
     while start <= end:
         mid = (start + end) // 2
-        if key(seq[mid]) == target:
-            while not (mid == 0 or key(seq[mid - 1]) != target):
-                mid -= 1
-            return mid
-        elif mid == 0 and key(seq[mid]) > target:
-            return mid
-        elif (
-            (key(seq[mid]) < target)
-            and mid + 1 < len(seq)
-            and key(seq[mid + 1] > target)
-        ):
-            return mid + 1
-        elif key(seq[mid]) < target:
+        if key(seq[mid]) < target:
             start = mid + 1
-        elif key(seq[mid]) > target:
+        elif mid != 0 and key(seq[mid - 1]) >= target:
             end = mid - 1
+        else:
+            return mid
     return -2
 
 
