3f30dccaba2015114f8845da9c9a74f4c90b4c8e
Art <artour.borisenko@ya.ru>
2025-04-12 22:53:56 +0300

    refactor

--- main/main/solution_n42_klumbi.py
+++ main/main/solution_n42_klumbi.py
@@ -19,7 +19,7 @@ def print_output(out, file):
         print(*line, file=file)
 
 
-def merge_overlapping_intervals(lines: Iterable):
+def merge_overlapping_intervals(lines: Iterable) -> Iterable[list[int]]:
     """CPU - O(n log(n))
     RAM - O(n)"""
     max_end = -math.inf
