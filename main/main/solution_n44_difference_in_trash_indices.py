42b788acf116f3f2cc1b12af95b1a40d4c62f57f
igornb <igornb@ya.ru>
2025-06-26 22:22:47 +0700

    ...

--- main/main/solution_n44_difference_in_trash_indices.py
+++ main/main/solution_n44_difference_in_trash_indices.py
@@ -1,32 +1,17 @@
-import numba
-
 MAX_VALUE=1000000
 
 def main():
-    """CPU - ???
-    RAM - ???"""
+    """CPU - O(n log(k) + n log(n))
+    RAM - O(n)"""
     with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
         n=int(inp.readline())
-        vals=list(map(int, inp.readline().split()))
+        vals=sorted(map(int, inp.readline().split()))
         k=int(inp.readline())
-        diffs_counts = count_diffs(vals)
-        cnt=0
-        for diff, diff_cnt in enumerate(diffs_counts):
-            if cnt+diff_cnt >= k:
-                print(diff, file=outp)
-                break
-            cnt+=diff_cnt
+        print(search(vals,k), file=outp)
 
-@numba.njit
-def count_diffs(vals):
-    diffs_counts = (MAX_VALUE + 1) * [0]
-    for idx, val in enumerate(vals):
-        for val2 in vals[idx + 1:]:
-            diff_cnt = abs(val2 - val)
-            diffs_counts[diff_cnt] += 1
-    return diffs_counts
-
-
+def search(v,k):
+    pass
+    #TODO: solve
 if __name__ == '__main__':
     main()
 
