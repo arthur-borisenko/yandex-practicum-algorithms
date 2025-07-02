827bf6570c63f2c3d06065b88de78852d69b8ac2
igornb <igornb@ya.ru>
2025-07-03 00:00:35 +0700

    refactored task 46(sprint 3 final task B)

--- main/main/sprint_3_final/solution_n46_efficent_quicksort.py
+++ main/main/sprint_3_final/solution_n46_efficent_quicksort.py
@@ -1,56 +1,84 @@
-def find_pivot(array, left, right):
-    return sorted((array[left + (right - left) // 2], array[left], array[right]))[1]
+from statistics import median
+from typing import MutableSequence, Any, Sequence
 
 
-def reorder(array, pivot, left, right, cmp_func):
-    """Reorders array items to make elements less than pivot in left part of array, bigger - in the right part.
-    CPU - O(n)
+def find_pivot(array: Sequence, left, right):
+    return median((array[left + (right - left) // 2], array[left], array[right]))
+
+
+def reorder(
+    array: MutableSequence,
+    pivot: Any,
+    left: int,
+    right: int,
+    cmp_func=lambda a, b: a - b,
+):
+    """Reorders array items (from left to right) to make elements less than pivot in left part of array, bigger - in the right part.
+    CPU - O(n=right-left)
     RAM - O(1)"""
-    left1 = left
-    right1 = right
-    while right > left:
+    left_copy = left
+    right_copy = right
+    while right_copy > left_copy:
         if left >= right:
             break
-        while cmp_func(array[left], pivot) == -1:
-            left += 1
-        while cmp_func(array[right], pivot) == 1:
-            right -= 1
-        array[left], array[right] = array[right], array[left]
+        while cmp_func(array[left_copy], pivot) <= -1:
+            left_copy += 1
+        while cmp_func(array[right_copy], pivot) >= 1:
+            right_copy -= 1
+        array[left_copy], array[right_copy] = array[right_copy], array[left_copy]
     center = None
-    while left1 <= right1:
-        if array[left1] == pivot:
-            center = left1
-        left1+=1
+    left_copy = left
+    right_copy = right
+    while left_copy <= right_copy:
+        if array[left_copy] == pivot:
+            center = left_copy
+        left_copy += 1
     return center
 
 
-def quicksort(array, left, right, cmp_func):
+def quicksort(
+    array: MutableSequence,
+    cmp_func=lambda a, b: a - b,
+    left: int = None,
+    right: int = None,
+):
     """Inplace quicksort.
     CPU - O(n log(n))
     RAM - O(log(n))
-    array - array with unique items.
-    cmp_func - function that compares two items."""
+
+    :param array: array with unique items to sort.
+    :param cmp_func: function that compares two items.
+    :param left: left border of an input array.
+    :param right: right border of an input array.
+    """
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array) - 1
     if left >= right:
         return
     pivot = find_pivot(array, left, right)
     center = reorder(array, pivot, left, right, cmp_func)
-    quicksort(array, left, center - 1, cmp_func)
-    quicksort(array, center + 1, right, cmp_func)
+    quicksort(array, cmp_func, left, center - 1)
+    quicksort(array, cmp_func, center + 1, right)
 
-def cmp(a, b):
-    n1, p1, f1, n2, p2, f2 = a[0], a[1], a[2], b[0], b[1], b[2]
-    if p1 < p2:
+
+def cmp(a: tuple[int, int, str], b: tuple[int, int, str]):
+    if a[0] < b[0]:
         return 1
-    if p1 > p2:
+    elif a[0] > b[0]:
         return -1
-    if f1 < f2:
+
+    if a[1] < b[1]:
         return -1
-    if f1 > f2:
+    elif a[1] > b[1]:
         return 1
-    if n1 < n2:
+
+    if a[2] < b[2]:
         return -1
-    if n1 > n2:
+    elif a[2] > b[2]:
         return 1
+
     return 0
 
 
@@ -61,9 +89,10 @@ def main():
         array = []
         for i in range(n):
             line = inp.readline()
-            array.append((line.split()[0], int(line.split()[1]), int(line.split()[2])))
-        quicksort(array, 0, n - 1, cmp)
-        print(*map(lambda x: x[0], array), sep="\n", file=outp)
+            data = line.split()
+            array.append((int(data[1]), int(data[2]), data[0]))
+        quicksort(array, cmp)
+        print(*map(lambda x: x[2], array), sep="\n", file=outp)
 
 
 if __name__ == "__main__":
