967d1b3fb82e999423b2c094dd25b0c19b33200f
igornb <igornb@ya.ru>
2025-07-05 00:06:43 +0700

    solved

---
+++ main/main/solution_n47_circles.py
@@ -0,0 +1,14 @@
+def main():
+    with open("input.txt") as inp, open("output.txt", "w") as out:
+        n = inp.readline()
+        d = []
+        occurrences = {}
+        for i in range(int(n)):
+            line = inp.readline().strip().rstrip()
+            occurrences[line] = occurrences.get(line, 0) + 1
+            if occurrences.get(line, 0) == 1:
+                print(line, file=out)
+
+
+if __name__ == "__main__":
+    main()
