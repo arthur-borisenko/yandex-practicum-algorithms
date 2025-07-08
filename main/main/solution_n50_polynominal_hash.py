f0f3740d44317828685c8a8602d722e17138953e
igornb <igornb@ya.ru>
2025-07-08 23:48:23 +0700

    refactored.

--- main/main/solution_n50_polynominal_hash.py
+++ main/main/solution_n50_polynominal_hash.py
@@ -2,14 +2,14 @@ def polynominal_hash(s, base, mod):
     """CPU - O(n)
     RAM - O(1)
 
-    we can use result%mod on all iterations, and it will equal result%mod on last iteration because:
+    we can use result % mod on all iterations, and it will equal result%mod on last iteration because:
     result = mod * a + r,
     so r = result % mod = result - mod * a.
     On next iteration we calculate result using:
     result = (base * mod * a + base * r + code) % mod
     and because first part has multiplier a, it will give zero modulo,
-    so (base * mod * a + base * r + code)%mod = (base * mod * a - base*mod*a+base*r+code)%mod
-    so, because we return result%mod, we can use (base*mod*a - base*mod*a+base*r+code)%mod,
+    so (base * mod * a + base * r + code) % mod = (base * mod * a - base*mod*a+base*r+code)%mod
+    so, because we return result % mod, we can use (base * mod * a - base * mod * a + base * r + code) % mod,
     which equals (base * (result % mod) + code) % mod in all calculations
     """
     result = 0
