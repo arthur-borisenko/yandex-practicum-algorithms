72fc5a3a28b2167ea0939c6e8eff82bff62846ba
igornb <igornb@ya.ru>
2025-07-12 22:50:34 +0700

    solved task 53.

---
+++ test/main/test_n55_mnogogosha.py
@@ -0,0 +1,33 @@
+import unittest
+
+import main.main.solution_n55_mnogogosha as solution
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+
+    def test_case1(self):
+        value = testUtil.file_test(
+            """10 2
+gggggooooogggggoooooogggggssshaa
+""",
+            solution.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """0 5
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """3 4
+allallallallalla
+""",
+            solution.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """0 1 2
+""".rstrip(),
+        )
