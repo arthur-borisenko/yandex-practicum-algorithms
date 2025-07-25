96e9cbcd3f63f2e925f53f9bf47ff981d1f34446
igornb <igornb@ya.ru>
2025-07-25 23:13:33 +0700

    ...

--- main/main/sprint_4_final/solution_n60_1_hash_map_open_addressation.py
+++ main/main/sprint_4_final/solution_n60_1_hash_map_open_addressation.py
@@ -4,7 +4,7 @@ class DeletedElement:
 
 
 class HashMap:
-    def _pf(self, b, i):
+    def _probing_func(self, b, i):
         return (b + i) % self._m
 
     def __init__(self, m=2 * 10**5, hash_fn=lambda x: abs(int(x))):
@@ -17,8 +17,8 @@ class HashMap:
     def __getitem__(self, key):
         i = 0
         bucket_i = self.hash_fn(key) % self._m
-        while self._pf(bucket_i, i) != bucket_i - 1:
-            bucket = self._arr[self._pf(bucket_i, i)]
+        while self._probing_func(bucket_i, i) != bucket_i - 1:
+            bucket = self._arr[self._probing_func(bucket_i, i)]
             if bucket[0] == key:
                 return bucket[1]
             elif bucket[0] is None:
@@ -30,15 +30,15 @@ class HashMap:
     def __setitem__(self, key, value):
         i = 0
         bucket_i = self.hash_fn(key) % self._m
-        while self._pf(bucket_i, i) != bucket_i - 1:
-            bucket = self._arr[self._pf(bucket_i, i)]
+        while self._probing_func(bucket_i, i) != bucket_i - 1:
+            bucket = self._arr[self._probing_func(bucket_i, i)]
             if bucket[0] == key:
                 bucket = (key, value)
-                self._arr[self._pf(bucket_i, i)] = bucket
+                self._arr[self._probing_func(bucket_i, i)] = bucket
                 return
             elif bucket[0] is None or isinstance(bucket[0], DeletedElement):
                 bucket = (key, value)
-                self._arr[self._pf(bucket_i, i)] = bucket
+                self._arr[self._probing_func(bucket_i, i)] = bucket
                 return
             else:
                 i += 1
@@ -47,10 +47,10 @@ class HashMap:
     def __delitem__(self, key):
         i = 0
         bucket_i = self.hash_fn(key) % self._m
-        while self._pf(bucket_i, i) != bucket_i - 1:
-            bucket = self._arr[self._pf(bucket_i, i)]
+        while self._probing_func(bucket_i, i) != bucket_i - 1:
+            bucket = self._arr[self._probing_func(bucket_i, i)]
             if bucket[0] == key:
-                self._arr[self._pf(bucket_i, i)] = (DeletedElement(), None)
+                self._arr[self._probing_func(bucket_i, i)] = (DeletedElement(), None)
                 return
             elif bucket[0] is None:
                 raise KeyError(key)
@@ -58,19 +58,18 @@ class HashMap:
                 i += 1
         raise KeyError(key)
 
-    def keys(self):
-        keys = []
-        for bucket in self._arr:
-            if bucket[0] is not None and not isinstance(bucket[0], DeletedElement):
-                keys.append(bucket[0])
-        return keys
-
-    def values(self):
-        values = []
-        for bucket in self._arr:
-            if bucket[0] is not None and not isinstance(bucket[0], DeletedElement):
-                values.append(bucket[1])
-        return values
+    def get(self, key, default=None):
+        i = 0
+        bucket_i = self.hash_fn(key) % self._m
+        while self._probing_func(bucket_i, i) != bucket_i - 1:
+            bucket = self._arr[self._probing_func(bucket_i, i)]
+            if bucket[0] == key:
+                return bucket[1]
+            elif bucket[0] is None:
+                return default
+            else:
+                i += 1
+        return default
 
     def pop(self, key):
         value = self[key]
@@ -86,18 +85,15 @@ def main():
             query = inp.readline().split()
             match query[0]:
                 case "get":
-                    try:
-                        i = query[1]
-                        print(data[i], file=out)
-                    except KeyError:
-                        print(None, file=out)
+                    i = int(query[1])
+                    print(data.get(i), file=out)
                 case "put":
-                    i = query[1]
-                    value = query[2]
+                    i = int(query[1])
+                    value = int(query[2])
                     data[i] = value
                 case "delete":
                     try:
-                        i = query[1]
+                        i = int(query[1])
                         print(data.pop(i), file=out)
                     except KeyError:
                         print(None, file=out)
