8eed771979f839e97443bb72642ba49a801c019e
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-07-26 00:45:04 +0700

    sprint 4 final tasks (#62)
    solved task A
    solved task B
    
    ---------
    
    Co-authored-by: igornb <igornb@ya.ru>

---
+++ test/main/sprint_4_final/test_n59_search_system.py
@@ -0,0 +1,48 @@
+import unittest
+
+import main.main.sprint_4_final.solution_n59_search_system as solution
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+
+    def test_case1(self):
+        value = testUtil.file_test(
+            """3
+i love coffee
+coffee with milk and sugar
+free tea for everyone
+3
+i like black coffee without milk
+everyone loves new year
+mary likes black coffee without milk
+""",
+            solution.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """1 2
+3
+2 1
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """6
+buy flat in moscow
+rent flat in moscow
+sell flat in moscow
+want flat in moscow like crazy
+clean flat in moscow on weekends
+renovate flat in moscow
+1
+flat in moscow for crazy weekends
+""",
+            solution.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """4 5 1 2 3
+""".rstrip(),
+        )
