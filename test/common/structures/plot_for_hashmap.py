8f08e2e6efa094902db736463950f3a3ca8185a7
igornb <igornb@ya.ru>
2025-07-26 22:58:35 +0700

    ...

---
+++ test/common/structures/plot_for_hashmap.py
@@ -0,0 +1,47 @@
+import time
+from matplotlib import pyplot
+import main.common.structures.hashmap as hm
+
+
+def o1(n):
+    return
+
+
+def on(n):
+    a = list(range(n)) * 100
+    return
+
+
+def on2(n):
+    a = 0
+    for i in range(n):
+        for j in range(n):
+            a += i + j
+
+
+def hmm(n):
+
+    a = 0
+    m = hm.HashMap()
+    for i in range(n):
+        m[i] = i * 2
+    for i in range(n):
+        a += m[i]
+
+
+def aaa(*ms):
+    for m, color in ms:
+        x = 1000000
+        points = {}
+        n = 1
+        while n < x:
+            n = n * 2
+            t_s = time.time()
+            m(n)
+            t_e = time.time()
+            points[n] = t_e - t_s
+        pyplot.plot(list(points.keys()), list(points.values()), color=color)
+    pyplot.show()
+
+
+aaa((o1, "grey"), (on, "black"), (hmm, "red"))
