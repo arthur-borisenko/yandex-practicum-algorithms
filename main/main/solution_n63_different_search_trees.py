bc3c9f2df9401cf7afb6b43b1854e411e9789b5e
igornb <igornb@ya.ru>
2025-07-27 23:14:56 +0700

    task 63 solved

--- main/main/solution_n63_different_search_trees.py
+++ main/main/solution_n63_different_search_trees.py
@@ -3,13 +3,17 @@ def solution(data):
         return 1
     if len(data) == 0:
         return 1
-    variants=0
+    variants = 0
     for i in range(len(data)):
-        left_data=data[:i]
-        right_data=data[(i+1):]
-        variants+=solution(left_data)*solution(right_data)
+        left_data = data[:i]
+        right_data = data[(i + 1) :]
+        variants += solution(left_data) * solution(right_data)
         pass
     return variants
+
+
 def main():
-    with open("../../test/main/input.txt") as inp, open("../../test/main/output.txt", "w") as out:
+    with open("input.txt") as inp, open(
+        "output.txt", "w"
+    ) as out:
         print(solution(list(range(int(inp.readline())))), file=out)
