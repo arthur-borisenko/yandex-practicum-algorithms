9885759fd5a34fcfc71d4f03208b388cb9350646
igornb <igornb@ya.ru>
2025-08-05 23:58:32 +0700

    task 71 solved

--- main/main/solution_n71_max_depth.py
+++ main/main/solution_n71_max_depth.py
@@ -1,57 +1,77 @@
 import os
 
-LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+LOCAL = os.environ.get("REMOTE_JUDGE", "false") != "true"
 
 if LOCAL:
+
     class Node:
-        def __init__(self, left=None, right=None, value=0, size=0):
+        def __init__(self, value, left=None, right=None):
+            self.value = value
             self.right = right
             self.left = left
-            self.value = value
-            self.size = size
 
-def direction(node, k):
-    if node.size < k:
-        return 'NE'
-    elif node.size == k:
-        return 'stop'
-    elif node.left is not None and node.left.size >= k:
-        return 'left'
-    else:
-        return 'right'
-def _split(parent_root, parent, root, k):
-    node = root
-    if direction(node, k) == "NE":
-        return root, None
-    elif direction(node, k) == "stop":
-        if parent is None:
-            return root, None
-        else:
-            if parent.left == root:
-                parent.left = None
-            else:
-                parent.right = None
-            return node, parent_root
-    elif direction(node, k) == "left":
-        return _split(parent_root, node, node.left, k)
-    else:
-        return _split(parent_root, node, node.right, k)
-def split(root, k):
+
+class Stack:
+    stack = []
+    __len__ = stack.__len__
+
+    def __init__(self):
+        self.stack = []
+        self.__len__ = self.stack.__len__
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError
+        return self.stack.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError
+        return self.stack[-1]
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+
+def children(node):
+    c = []
+    if node.left is not None:
+        c.append(node.left)
+    if node.right is not None:
+        c.append(node.right)
+    return c
+
+
+def solution(root) -> int:
     stack = Stack()
-    pass
+    node = root
+    stack.push((node, 0))
+    max_depth = 0
+    while not stack.isEmpty():
+        node, node_depth = stack.pop()
+        if len(children(node)) == 0:
+            max_depth = max(node_depth, max_depth)
+        for node in children(node):
+            stack.push((node, node_depth + 1))
+    #  “ヽ(´▽｀)ノ”
+    return max_depth + 1
 
 
 def test():
-    node1 = Node(None, None, 3, 1)
-    node2 = Node(None, node1, 2, 2)
-    node3 = Node(None, None, 8, 1)
-    node4 = Node(None, None, 11, 1)
-    node5 = Node(node3, node4, 10, 3)
-    node6 = Node(node2, node5, 5, 6)
-    left, right = split(node6, 4)
-    assert (left.size == 4)
-    assert (right.size == 2)
+    node1 = Node(1, None, None)
+    node2 = Node(4, None, None)
+    node3 = Node(3, node1, node2)
+    node4 = Node(8, None, None)
+    node5 = Node(5, node3, node4)
+
+    assert solution(node5) == 3
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
