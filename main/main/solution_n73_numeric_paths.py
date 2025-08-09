670ed6f788edb8ae2fe88589fb5975044559110c
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-08-09 22:56:35 +0700

    task 73 solved (#76)
    * solved
    
    ---------
    
    Co-authored-by: igornb <igornb@ya.ru>

---
+++ main/main/solution_n73_numeric_paths.py
@@ -0,0 +1,73 @@
+import os
+
+LOCAL = os.environ.get("REMOTE_JUDGE", "false") != "true"
+
+if LOCAL:
+
+    class Node:
+        def __init__(self, value, left=None, right=None):
+            self.value = value
+            self.right = right
+            self.left = left
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError("Stack is empty")
+        return self.stack.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError
+        return self.stack[-1]
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+
+def children(node):
+    c = []
+    if node.left is not None:
+        c.append(node.left)
+    if node.right is not None:
+        c.append(node.right)
+    return c
+
+
+def solution(root) -> int:
+    res = 0
+    stack = Stack()
+    node = root
+    stack.push((node, str(node.value)))
+    while not stack.isEmpty():
+        node, np = stack.pop()
+        if node.left is None and node.right is None:
+            res += int(np)
+        for node in children(node):
+            stack.push((node, np + str(node.value)))
+    return res
+    #  “ヽ(´▽｀)ノ”
+
+
+def test():
+    node1 = Node(2, None, None)
+    node2 = Node(1, None, None)
+    node3 = Node(3, node1, node2)
+    node4 = Node(2, None, None)
+    node5 = Node(1, node4, node3)
+
+    assert solution(node5) == 275
+
+
+if __name__ == "__main__":
+    test()
