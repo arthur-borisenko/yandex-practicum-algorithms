0fc02808eb589cbb27756aa529442a32d8481141
Art <artour.borisenko@ya.ru>
2025-08-13 21:23:18 +0300

    done

--- main/main/sprint_5_final/solution_n76_remove_node.py
+++ main/main/sprint_5_final/solution_n76_remove_node.py
@@ -1,7 +1,6 @@
 from typing import Optional
 import os
 
-
 LOCAL = os.environ.get("REMOTE_JUDGE", "false") != "true"
 
 if LOCAL:
@@ -43,28 +42,31 @@ def prepare_replacement_node(root):
         parent.right = node.left
     return node
 
+
 def replace_node_link(parent, node, new_node):
     if parent.left is node:
         parent.left = new_node
     else:
         parent.right = new_node
+
+
 def remove(root, key) -> Optional[Node]:
     if root is None:
         return None
     parent, node = find_key(root, key)
     if node is None:
-        return root # Если вершины нет в дереве, изменять его не требуется
-    if root.left is None and root.right is None: # Если дерево состояло из одной вершины, то после её удаления дерева не останется.
+        return root  # Если вершины нет в дереве, изменять его не требуется
+    if root.left is None and root.right is None:  # Если дерево состояло из одной вершины, то после её удаления дерева не останется.
         return None
     right = node.right
     left = node.left
-    if left is None and right is None: # Если мы удаляем лист, то дерево останется одним деревом и не распадётся на части.
+    if left is None and right is None:  # Если мы удаляем лист, то дерево останется одним деревом и не распадётся на части.
         replace_node_link(parent, right, None)
-    if left is not None and right is not None: # Если мы удаляем корень, у которого есть оба поддерева, то каждое поддерево станет отдельным деревом. Если мы удаляем вершину, у которой есть оба ребёнка и родитель, то дерево распадётся на родительское и два поддерева.
+    if left is not None and right is not None:  # Если мы удаляем корень, у которого есть оба поддерева, то каждое поддерево станет отдельным деревом. Если мы удаляем вершину, у которой есть оба ребёнка и родитель, то дерево распадётся на родительское и два поддерева.
         node_to_replace = prepare_replacement_node(left)
         node_to_replace.right = right
         node_to_replace.left = left
-    else: # Только 1 ребенок - присоединяем его вместо узла
+    else:  # Только 1 ребенок - присоединяем его вместо узла
         node_to_replace = right if right is not None else left
     if parent is not None:
         replace_node_link(parent, node, node_to_replace)
@@ -91,9 +93,7 @@ def test():
     n3.right = n7
 
     root = n1
-    TreeVisualizer(root).display()
     remove(root, 1)
-    TreeVisualizer(root).display()
 
 
 if __name__ == "__main__":
