48673319db39cbbeb930c70794a82c51fb4c7752
Art <artour.borisenko@ya.ru>
2025-08-13 22:14:17 +0300

    done

--- main/main/sprint_5_final/solution_n76_remove_node.py
+++ main/main/sprint_5_final/solution_n76_remove_node.py
@@ -1,3 +1,5 @@
+import sys
+import time
 from typing import Optional
 import os
 
@@ -40,7 +42,9 @@ def prepare_replacement_node(root):
         node = node.right
     if parent is not None:
         parent.right = node.left
-    return node
+    if parent is None:
+        return None, node
+    return root, node
 
 
 def replace_node_link(parent, node, new_node):
@@ -63,96 +67,35 @@ def remove(root, key) -> Optional[Node]:
     if left is None and right is None:  # Если мы удаляем лист, то дерево останется одним деревом и не распадётся на части.
         replace_node_link(parent, node, None)
         return root
-    if left is not None and right is not None:  # Если мы удаляем корень, у которого есть оба поддерева, то каждое поддерево станет отдельным деревом. Если мы удаляем вершину, у которой есть оба ребёнка и родитель, то дерево распадётся на родительское и два поддерева.
-        node_to_replace = prepare_replacement_node(left)
+    elif left is not None and right is not None:  # Если мы удаляем корень, у которого есть оба поддерева, то каждое поддерево станет отдельным деревом. Если мы удаляем вершину, у которой есть оба ребёнка и родитель, то дерево распадётся на родительское и два поддерева.
+        left, node_to_replace = prepare_replacement_node(left)
         node_to_replace.right = right
         node_to_replace.left = left
+        if parent is not None:
+            replace_node_link(parent, node, node_to_replace)
+        else:
+            root = node_to_replace
     else:  # Только 1 ребенок - присоединяем его вместо узла
         node_to_replace = right if right is not None else left
-    if parent is not None:
-        replace_node_link(parent, node, node_to_replace)
-    else:
-        root = node_to_replace
+        if parent is not None:
+            replace_node_link(parent, node, node_to_replace)
+        else:
+            root = node_to_replace
     return root
     #  “ヽ(´▽｀)ノ”
-
-class Stack:
-  def __init__(self):
-    self.stack = []
-
-  def push(self, element):
-    self.stack.append(element)
-
-  def pop(self):
-    if self.isEmpty():
-      return "Stack is empty"
-    return self.stack.pop()
-
-  def peek(self):
-    if self.isEmpty():
-      return "Stack is empty"
-    return self.stack[-1]
-
-  def isEmpty(self):
-    return len(self.stack) == 0
-
-  def size(self):
-    return len(self.stack)
-
-
-
-
-def children(node):
-    c = []
-    if node.left is not None:
-        c.append(node.left)
-    if node.right is not None:
-        c.append(node.right)
-    return c
-def dfs(root):
-    res=[]
-    stack=Stack()
-    node = root
-    stack.push(node)
-    while not stack.isEmpty():
-        node = stack.pop()
-        yield node.value
-        for node in children(node):
-            stack.push(node)
-    return res
-
 def test():
-    """Data equivalent of correctness
-7
-1 4 2 3
-2 2 4 5
-3 6 6 7
-4 1 -1 -1
-5 3 -1 -1
-6 5 -1 -1
-7 7 -1 -1
-1"""
-    n1 = Node(value=4)
-    n2 = Node(value=2)
-    n3 = Node(value=6)
-    n4 = Node(value=1)
-    n5 = Node(value=3)
-    n6 = Node(value=5)
-    n7 = Node(value=7)
+    node1 = Node(None, None, 2)
+    node2 = Node(node1, None, 3)
+    node3 = Node(None, node2, 1)
+    node4 = Node(None, None, 6)
+    node5 = Node(node4, None, 8)
+    node6 = Node(node5, None, 10)
+    node7 = Node(node3, node6, 5)
+    new_head = remove(node7, 10)
+    assert new_head.value == 5
+    assert new_head.right is node5
+    assert new_head.right.value == 8
 
-    n1.left = n2
-    n1.right = n3
-    n2.left = n4
-    n2.right = n5
-    n3.left = n6
-    n3.right = n7
-
-    root = n1
-    input1=list(dfs(root))
-    print(*input1)
-    remove(root, 1)
-    output1=list(dfs(root))
-    print(*output1)
 
 
 if __name__ == "__main__":
