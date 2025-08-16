706868b28b7a565db92a6e7e727ece2a7fa1a9e2
Art <artour.borisenko@ya.ru>
2025-08-17 00:39:32 +0300

    task 82 solved

--- main/main/solution_n81_topological_sort.py
+++ main/main/solution_n81_topological_sort.py
@@ -1,4 +1,13 @@
-class VertexAbstraction:
+import sys
+
+
+class ColorEnum:
+    WHITE = 1
+    GREY = 2
+    BLACK = 3
+
+
+class Vertex:
     def __init__(self, id, ribble_map):
         self.data = ribble_map
         self.id = id
@@ -7,7 +16,7 @@ class VertexAbstraction:
     def connections(self):
         ribbles_for_node = self.data.get(self.id, {})
         for second in sorted(ribbles_for_node.keys(), reverse=True):
-            yield VertexAbstraction(second, self.data), ribbles_for_node[second]
+            yield Vertex(second, self.data), ribbles_for_node[second]
 
     def hasChild(self, child):
         return child in self.connections
@@ -28,31 +37,31 @@ def parse_input(inp):
 def _top_sort(start, ribble_map, colors):
     stack = []
     d = []
-    stack.append(VertexAbstraction(start, ribble_map))
-    colors[start] = "white"
+    stack.append(Vertex(start, ribble_map))
+    colors[start] = ColorEnum.WHITE
     while stack:
         vertex = stack.pop()
-        if colors.get(vertex.id, "white") == "grey":
+        if colors.get(vertex.id, ColorEnum.WHITE) == ColorEnum.GREY:
             yield vertex.id
-            colors[vertex.id] = "black"
-        if colors.get(vertex.id, "white") == "white":
+            colors[vertex.id] = ColorEnum.BLACK
+        if colors.get(vertex.id, ColorEnum.WHITE) == ColorEnum.WHITE:
             stack.append(vertex)
-            colors[vertex.id] = "grey"
+            colors[vertex.id] = ColorEnum.GREY
             for next_vertex, weight in vertex.connections:
-                if colors.get(next_vertex.id, "white") == "white":
+                if colors.get(next_vertex.id, ColorEnum.WHITE) == ColorEnum.WHITE:
                     stack.append(next_vertex)
 
 
 def top_sort(ribble_map, n):
     colors = {}
     for start in range(1, n + 1):
-        if colors.get(start, "white") == "white":
+        if colors.get(start, ColorEnum.WHITE) == ColorEnum.WHITE:
             yield from _top_sort(start, ribble_map, colors)
 
 
 def main():
     m, n, k = parse_input(open("input.txt", "r"))
-    print(*reversed(list(top_sort(m, n))))
+    print(*reversed(list(top_sort(m, n))), file=open("output.txt", "w"))
 
 
 if __name__ == "__main__":
