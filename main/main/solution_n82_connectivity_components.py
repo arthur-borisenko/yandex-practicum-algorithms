7c78d8daa12da7d8a4269a91caf7a0c1605a0274
Art <artour.borisenko@ya.ru>
2025-08-16 23:49:56 +0300

    task 83 solved

--- main/main/solution_n82_connectivity_components.py
+++ main/main/solution_n82_connectivity_components.py
@@ -1,7 +1,13 @@
 from collections import defaultdict
 
 
-class VertexAbstraction:
+class ColorEnum:
+    WHITE = 1
+    GREY = 2
+    BLACK = 3
+
+
+class Vertex:
     def __init__(self, id, ribble_map):
         self.data = ribble_map
         self.id = id
@@ -10,7 +16,7 @@ class VertexAbstraction:
     def connections(self):
         ribbles_for_node = self.data.get(self.id, {})
         for second in sorted(ribbles_for_node.keys(), reverse=True):
-            yield VertexAbstraction(second, self.data), ribbles_for_node[second]
+            yield Vertex(second, self.data), ribbles_for_node[second]
 
     def hasChild(self, child):
         return child in self.connections
@@ -29,46 +35,40 @@ def parse_input(inp):
     return m, n, k
 
 
-def ks(start, ribble_map, colors, ksids):
-    iteration_id = max(ksids.values()) + 1 if len(ksids) > 0 else 0
-    stack = []
-    visited = set()
-    d = []
-    stack.append(VertexAbstraction(start, ribble_map))
-    colors[start] = "white"
-    while stack:
-        vertex = stack.pop()
-        if colors.get(vertex.id, "white") == "grey":
-            visited.add(vertex.id)
-            iteration_id = ksids.get(vertex.id, iteration_id)
-            colors[vertex.id] = "black"
-        if colors.get(vertex.id, "white") == "white":
-            stack.append(vertex)
-            colors[vertex.id] = "grey"
-            for next_vertex, weight in vertex.connections:
-                if colors.get(next_vertex.id, "white") == "white":
-                    stack.append(next_vertex)
-    for node in visited:
-        ksids[node] = iteration_id
-
-
-def kss(ribble_map, n):
+def solution(ribble_map, n):
     colors = {}
     ksids = {}
     for start in range(1, n + 1):
-        if colors.get(start, "white") == "white":
-            ks(start, ribble_map, colors, ksids)
+        if colors.get(start, ColorEnum.WHITE) == ColorEnum.WHITE:
+            iteration_id = max(ksids.values()) + 1 if len(
+                ksids) > 0 else 0
+            stack = []
+            visited = set()
+            stack.append(Vertex(start, ribble_map))
+            colors[start] = ColorEnum.WHITE
+            while stack:
+                vertex = stack.pop()
+                if colors.get(vertex.id,
+                              ColorEnum.WHITE) == ColorEnum.GREY:
+                    visited.add(vertex.id)
+                    iteration_id = ksids.get(vertex.id, iteration_id)
+                    colors[vertex.id] = ColorEnum.BLACK
+                if colors.get(vertex.id,
+                              ColorEnum.WHITE) == ColorEnum.WHITE:
+                    stack.append(vertex)
+                    colors[vertex.id] = ColorEnum.GREY
+                    for next_vertex, weight in vertex.connections:
+                        if colors.get(next_vertex.id,
+                                      ColorEnum.WHITE) == ColorEnum.WHITE:
+                            stack.append(next_vertex)
+            for node in visited:
+                ksids[node] = iteration_id
     ksids2 = defaultdict(list)
     for vx in ksids.keys():
         ksids2[ksids[vx]].append(vx)
     return ksids2.values()
 
 
-def main():
-    m, n, k = parse_input(open("input.txt", "r"))
-    data = kss(m, n)
-    print(len(data), *map(lambda x: " ".join(map(str, sorted(x))), data), sep="\n")
-
-
-if __name__ == "__main__":
-    main()
+m, n, k = parse_input(open("input.txt", "r"))
+data = solution(m, n)
+print(len(data), *map(lambda x: " ".join(map(str, sorted(x))), data), sep="\n")
