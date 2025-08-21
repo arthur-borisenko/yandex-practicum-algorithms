cf30fa0352199d806a52b473b8a737d6d0572204
Art <artour.borisenko@ya.ru>
2025-08-21 22:56:39 +0300

    sprint 6 final

--- main/main/sprint_6_final/solution_n89_water_world.py
+++ main/main/sprint_6_final/solution_n89_water_world.py
@@ -1,85 +1,63 @@
-from queue import Queue
+import time
 
 
 class ColorEnum:
-    WHITE = 0
-    GREY = 1
-
-
+    WHITE: int = 0
+    GREY: int = 1
 class Gh:
-    def __init__(self, mtx, vertices):
-        self.vertices = vertices
-        self.mtx = mtx
-
-    def __len__(self):
-        return len(self.vertices)
-
-    def __iter__(self):
-        return iter(self.vertices)
-
-    def outgoing_edges(self, v):
-        x, y = xyid(v)
-        res = []
+    def __init__(self, mtx: list[list[bool]]):
+        self.mtx:list[list[bool]] = mtx
+    def outgoing_edges(self, v) -> list[int]:
+        result = v // 10000, v % 10000
+        x, y = result
+        res: list[int]=[]
         if x - 1 >= 0 and self.mtx[y][x - 1]:
-            res.append(vid(x - 1, y))
+            x1 = x - 1
+            res.append(10000 * x1 + y)
         if x + 1 < len(self.mtx[y]) and self.mtx[y][x + 1]:
-            res.append(vid(x + 1, y))
+            x2 = x + 1
+            res.append(10000 * x2 + y)
         if y - 1 >= 0 and self.mtx[y - 1][x]:
-            res.append(vid(x, y - 1))
+            y1 = y - 1
+            res.append(10000 * x + y1)
         if y + 1 < len(self.mtx) and self.mtx[y + 1][x]:
-            res.append(vid(x, y + 1))
+            y2 = y + 1
+            res.append(10000 * x + y2)
         return res
 
 
-def vid(x, y):
-    return 10000 * x + y
-
-
-def xyid(v):
-    return v // 10000, v % 10000
-
-
 def subgraph_size(start, graph: Gh, visited: set):
-    queue = Queue()
-    queue.put(start)
-    res = 0
-    while not queue.empty():
-        vertex = queue.get()
+    res=0
+    stack=[]
+    stack.append(start)
+    while stack:
+        vertex = stack.pop()
         if vertex not in visited:
-            visited.add(vertex)
             res += 1
-            for next_vertex in graph.outgoing_edges(vertex):
-                if next_vertex not in visited:
-                    queue.put(next_vertex)
+            visited.add(vertex)
+            for neighbor in graph.outgoing_edges(vertex):
+                if neighbor not in visited:
+                    stack.append(neighbor)
     return res
-
 
 def aaa(inp):
     res = []
     n, m = map(int, inp.readline().split())
     for i in range(n):
-        res.append(list(map(lambda x: x == "#", inp.readline().strip())))
-    r = []
-
+        res.append(
+            list(map(lambda x: x == "#", inp.readline().strip())))
+    graph = Gh(res)
+    l, s = 0, 0
+    visited = set()
     for y in range(n):
         for x in range(m):
             if res[y][x]:
-                r.append(vid(x, y))
-    graph = Gh(res, r)
-    l, s = 0, 0
-    visited = set()
-    for start in graph.vertices:
-        if start in visited:
-            continue
-        sgs = subgraph_size(start, graph, visited)
-        l += 1
-        s = max(s, sgs)
+                start= 10000 * x + y
+                if start not in visited:
+                    sgs=subgraph_size(start, graph, visited)
+                    l+=1
+                    s=max(s, sgs)
     return l, s
 
+print(*aaa(open("input.txt", "r")))
 
-def main():
-    print(*aaa(open("input.txt", "r")))
-
-
-if __name__ == "__main__":
-    main()
