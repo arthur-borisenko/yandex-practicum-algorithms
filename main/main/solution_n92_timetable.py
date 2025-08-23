74d8a364435165484092380b16315bfb8127ab20
Art <artour.borisenko@ya.ru>
2025-08-23 22:49:20 +0300

    ...

---
+++ main/main/solution_n92_timetable.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def pi(inp):
+    r = []
+    n = int(inp.readline())
+    for i in range(n):
+        a, b = inp.readline().split()
+        aa = a.split(".")
+        bb = b.split(".")
+        if len(aa) == 1:
+            aaa = int(aa[0]) * 60
+        else:
+            aaa = int(aa[0]) * 60 + int(aa[1])
+        if len(bb) == 1:
+            bbb = int(bb[0]) * 60
+        else:
+            bbb = int(bb[0]) * 60 + int(bb[1])
+        r.append((aaa, bbb))
+    return r
+
+
+def main():
+    with open("input.txt") as inp, open("output.txt", "w") as out:
+        rr = pi(inp)
+        rr1 = sorted(rr, key=lambda x: (x[1], x[0]))
+        rrr = []
+        t = 0
+        for el in rr1:
+            if el[0] >= t:
+                t = el[1]
+                rrr.append(
+                    list(map(lambda x: f"{x // 60}.{x % 60}", el)))
+        print(len(rrr), file=out)
+        for v in rrr:
+            print(*v, file=out)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/main/test_n92_timetable.py
@@ -0,0 +1,74 @@
+import unittest
+import main.main.solution_n92_timetable as task
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """5
+9.0 10.0
+9.3 10.3
+10.0 11.0
+10.3 11.3
+11.0 12.0
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """3
+9.0 10.0
+10.0 11.0
+11.0 12.0
+
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """3
+9.0 10.0
+11.0 12.25
+12.15 13.3
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """2
+9.0 10.0
+11.0 12.25
+
+""".rstrip(),
+        )
+
+    def test_case3(self):
+        value = testUtil.file_test(
+            """7
+19.0 19.0
+7.0 14.0
+12.0 14
+8.0 22.0
+22.0 23.0
+5.0 21.0
+9.0 23.0
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """3
+7.0 14.0
+19.0 19.0
+22.0 23.0
+
+""".rstrip(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
