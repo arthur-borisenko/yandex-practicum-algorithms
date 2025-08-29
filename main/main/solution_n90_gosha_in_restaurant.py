231d6b2bc93521b499bcc98e7124107eeaf2abf7
Art <artour.borisenko@ya.ru>
2025-08-29 23:13:02 +0300

    task 99

--- main/main/solution_n90_gosha_in_restaurant.py
+++ main/main/solution_n90_gosha_in_restaurant.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+from io import StringIO
 
 m=defaultdict(dict)
 def a(ds, i, cu):
@@ -29,13 +30,41 @@ def a(ds, i, cu):
         sm3, cu3, spt3 = sm1, cu1, spt1
     m[i][cu] = (sm3, cu3, spt3)
     return sm3, cu3, spt3
-
 inp=sys.stdin
+inp=StringIO("""10
+169
+258
+406
+642
+844
+562
+889
+743
+144
+682
+""")
 n=int(inp.readline())
 l=[]
 for i in range(n):
     l.append(int(inp.readline()))
 #inp = [696, 744, 517, 510, 566, 772, 988, 691, 800, 802]
+d=[[(None, None, None) for _ in range(n)] for _ in range(n)]
+for i in range(n-1, 1, -1):
+    for j in range(len(d[i]) - 1):
+        if i + 1 == len(d):
+            d[i][j] = (0, j-1, [i]) if j > 0 else (l[i], j, [])
+        else:
+            if l[i]>500 and j<len(d[i+1])-1:
+                sm1, cu1, spt1=d[i+1][j+1]
+            else:
+                sm1, cu1, spt1=d[i+1][j]
+            sm1+=l[i]
+            sm2, cu2, spt2=d[i+1][j-1]
+            if sm2 is not None and sm2 < sm1:
+                sm3, cu3, spt3 = sm2, cu2, spt2 + [i + 1]
+            else:
+                sm3, cu3, spt3 = sm1, cu1, spt1
+            d[i][j] = (sm3, cu3, spt3)
 dt = a(l, 0, 0)
 print(dt[0], len(dt[2]))
 print(*sorted(dt[2]), sep="\n")
