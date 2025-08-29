8bc31678e5e9ecfc8c73233a9a8b51f52b2e2281
Art <artour.borisenko@ya.ru>
2025-08-29 21:36:30 +0300

    task 99 recursive

--- main/main/solution_n90_gosha_in_restaurant.py
+++ main/main/solution_n90_gosha_in_restaurant.py
@@ -1,51 +1,41 @@
 import sys
-from io import StringIO
+from collections import defaultdict
 
-inp=StringIO("""6
-1
-2
-6
-1
-42
-52
-""")
+m=defaultdict(dict)
+def a(ds, i, cu):
+    if cu in m[i]:
+        return m[i][cu]
+    if len(ds) - i <= 0:
+        m[i][cu] = (0, cu, [])
+        return 0, cu, []
+    if len(ds) - i <= cu:
+        v  = 0, cu - (len(ds) - i), list(range(i + 1, len(ds) + 1))
+        m[i][cu] = v
+        return v
+    cp = ds[i]
+    if cp > 500:
+        sm1, cu1, spt1 = a(ds, i + 1, cu + 1)
+    else:
+        sm1, cu1, spt1 = a(ds, i + 1, cu)
+    sm1 += cp
+    dt = a(ds, i + 1,
+                       cu - 1) if cu > 0 else (None, None, None)
+    sm2, cu2, spt2 = dt[0], dt[1], dt[2]
+    if not isinstance(sm2, int):
+        pass #print(sm2)
+    if sm2 is not None and sm2 < sm1:
+        sm3, cu3, spt3 = sm2, cu2, spt2 + [i + 1]
+    else:
+        sm3, cu3, spt3 = sm1, cu1, spt1
+    m[i][cu] = (sm3, cu3, spt3)
+    return sm3, cu3, spt3
+
 inp=sys.stdin
 n=int(inp.readline())
 l=[]
 for i in range(n):
-    l.append((int(inp.readline()), i))
-import heapq
-T=500
-l2=[[]]
-h1=[]=[]
-for el, i in l:
-    heapq.heappush(h1, (el, i))
-    l2.append(l2[-1].copy())
-    if el>T:
-        heapq.heappush(l2[-1], (el, i))
-l3=sorted(l, reverse=True)
-used=set()
-a=[]
-b=[]
-c=[]
-for el, i in l3:
-    vg=l2[i]
-    if len(vg)==0 or el in used:
-        a.append((el, i))
-    else:
-        vg1=heapq.heappop(vg)
-        g=True
-        while vg1 in used:
-            if len(vg)==0:
-                g=False
-                break
-            vg1=heapq.heappop(vg)
-        if g:
-            b.append((el, i, vg1))
-            c.append(i+1)
-            used.add(vg1)
-            used.add((el, i))
-        else:
-            a.append((el, i))
-print(sum(map(lambda x: x[0], a)), len(c))
-print(*sorted(c))
+    l.append(int(inp.readline()))
+#inp = [696, 744, 517, 510, 566, 772, 988, 691, 800, 802]
+dt = a(l, 0, 0)
+print(dt[0], len(dt[2]))
+print(*sorted(dt[2]), sep="\n")
