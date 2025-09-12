27105dea3bc6ac615c3d880f86d5928fecaa9a18
Art <artour.borisenko@ya.ru>
2025-09-12 22:16:35 +0300

    task 104 solved

--- main/main/solution_n104_alla_on_algosses.py
+++ main/main/solution_n104_alla_on_algosses.py
@@ -2,19 +2,19 @@ def main():
     with open("input.txt") as inp, open("output.txt", "w") as out:
         m = int(inp.readline())
         n = int(inp.readline())
-        dd = list(map(int, inp.readline().split()))
-        ddd = [-1 for _ in range(m + 1)]
-        ddd[0] = 0
-        for i in range(1, m + 1):
-            rr = -1
-            for d in dd:
-                if i < d or ddd[i - d] == -1:
+        nominals = list(map(int, inp.readline().split()))
+        dp = [-1 for _ in range(m + 1)]
+        dp[0] = 0
+        for sum_needed in range(1, m + 1):
+            min_banknotes_cnt = -1
+            for nominal in nominals:
+                if sum_needed < nominal or dp[sum_needed - nominal] == -1:
                     continue
-                r = ddd[i - d] + 1
-                if rr == -1 or r < rr:
-                    rr = r
-            ddd[i] = rr
-        print(ddd[-1], file=out)
+                current_banknotes_cnt = dp[sum_needed - nominal] + 1
+                if min_banknotes_cnt == -1 or current_banknotes_cnt < min_banknotes_cnt:
+                    min_banknotes_cnt = current_banknotes_cnt
+            dp[sum_needed] = min_banknotes_cnt
+        print(dp[-1], file=out)
 
 
 if __name__ == "__main__":
