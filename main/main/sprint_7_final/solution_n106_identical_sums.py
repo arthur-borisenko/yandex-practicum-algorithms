e6a7c8257d7ef143c7bf3e813af7ffa9572669b4
Art <artour.borisenko@ya.ru>
2025-09-14 18:23:40 +0300

    sprint 7 final

--- main/main/sprint_7_final/solution_n106_identical_sums.py
+++ main/main/sprint_7_final/solution_n106_identical_sums.py
@@ -1,21 +1,31 @@
-from collections import defaultdict
+def main():
+    with open("input.txt", "r") as inp, open("output.txt", "w") as out:
+        n = int(inp.readline())
+        l = list(map(int, inp.readline().split()))
+        s = sum(l)
+        if s % 2 == 0:
+            half_sum = s // 2
+            d0 = [0 for _ in range(half_sum)]
+            for i in range(n):
+                d1 = [0 for _ in range(half_sum)]
+                for j in range(1, half_sum + 1):
+                    nt = d0[j - 1] if i > 0 else 0
+                    t_mw = j - l[i]
+                    t = (
+                        ((l[i]) + (d0[t_mw - 1] if i > 0 and t_mw > 0 else 0))
+                        if t_mw >= 0
+                        else 0
+                    )
+                    if t > nt:
+                        d1[j - 1] = t
+                    else:
+                        d1[j - 1] = nt
+                d0 = d1
+            print(d0[-1] == half_sum, file=out)
 
-n = int(input())
-l = list(map(int, input().split()))
-s = sum(l)
-a = defaultdict(int)
-for el in l:
-    a[el] += 1
-if s % 2 == 0:
-    m = s // 2
-    dd = [{-1: 0} for i in range(m)]
-    dd[0][-1] = True
-    for i in range(m):
-        for el in l:
-            if i - el >= 0 and dd[i - el][-1] != 0 and a[el] > dd[i - el][el]:
-                dd[i][-1] = 1
-                dd[i][el] = dd[i].get(el, 0) + 1
-                break
-    print(dd[-1])
-else:
-    print(False)
+        else:
+            print(False, file=out)
+
+
+if __name__ == "__main__":
+    main()
