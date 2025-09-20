f0a8c283d9276445b2f985f6ff45199373e7054c
Art <artour.borisenko@ya.ru>
2025-09-20 22:11:52 +0300

    task 110 solved

---
+++ test/main/test_n110_compare_two_strings.py
@@ -0,0 +1,54 @@
+import unittest
+import main.main.solution_n110_compare_two_strings as task
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """gggggbbb
+bbef
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """-1
+
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """z
+aaaaaaa
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """1
+
+""".rstrip(),
+        )
+
+    def test_case3(self):
+        value = testUtil.file_test(
+            """ccccz
+aaaaaz
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """0
+
+""".rstrip(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
