4f34a6a2584228888c5cae821043229f34cbd7ce
Art <artour.borisenko@ya.ru>
2025-09-28 18:39:24 +0300

    task 118 solved

---
+++ test/main/test_n118_string_insertion.py
@@ -0,0 +1,42 @@
+import unittest
+import main.main.solution_n118_string_insertion as task
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """abacaba
+3
+queue 2
+deque 0
+stack 7
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """dequeabqueueacabastack
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """kukareku
+2
+p 1
+q 2
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """kpuqkareku
+""".rstrip(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
