85d4bc6cb0d75af1223a7f7c06abfb9ab7b9ea59
Art <artour.borisenko@ya.ru>
2025-10-04 21:51:02 +0300

    task 120 solved

--- main/main/sprint_8_final/solution_n119_packed_prefix.py
+++ main/main/sprint_8_final/solution_n119_packed_prefix.py
@@ -1,30 +1,34 @@
 def find_closing_bracket(s, i):
-    cnt=0
+    cnt = 0
     for ii in range(i, len(s)):
-        ss=s[ii]
-        if ss=="[" and ii>0:
+        ss = s[ii]
+        if ss == "[" and ii > 0:
             cnt += 1
-        elif ss=="]":
+        elif ss == "]":
             cnt -= 1
-        if cnt==0:
+        if cnt == 0:
             return ii
+
+
 def decompress(s):
-    res=[]
-    i=0
-    while i<len(s):
-        if s[i]=="[" and i>0 and s[i-1].isdigit():
-            ni=find_closing_bracket(s, i)
-            cnt=int(s[i-1])
-            res.append(decompress(s[i + 1:ni]) * cnt)
-            i=ni
-        elif (not s[i].isdigit()) or (i+1>=len(s)) or s[i+1]!="[":
+    res = []
+    i = 0
+    while i < len(s):
+        if s[i] == "[" and i > 0 and s[i - 1].isdigit():
+            ni = find_closing_bracket(s, i)
+            cnt = int(s[i - 1])
+            res.append(decompress(s[i + 1 : ni]) * cnt)
+            i = ni
+        elif (not s[i].isdigit()) or (i + 1 >= len(s)) or s[i + 1] != "[":
             res.append(s[i])
-        i+=1
+        i += 1
     return "".join(res)
+
+
 def main():
     with open("input.txt") as inp, open("output.txt", "w") as out:
-        n=int(inp.readline())
-        ss=[]
+        n = int(inp.readline())
+        ss = []
         for i in range(n):
             ss.append(decompress(inp.readline()))
         p = ss.pop()
@@ -33,12 +37,13 @@ def main():
             if s[0] != p[0]:
                 p = []
                 break
-            i=0
+            i = 0
             for i in range(len(p)):
-                if i + 1 >= len(s) or i + 1 >= len(p) or p[i + 1] != s[
-                    i + 1]:
+                if i + 1 >= len(s) or i + 1 >= len(p) or p[i + 1] != s[i + 1]:
                     break
-            p = p[:i + 1]
+            p = p[: i + 1]
         print("".join(p), file=out)
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- main/main/sprint_8_final/solution_n120_shpargalka.py
+++ main/main/sprint_8_final/solution_n120_shpargalka.py
@@ -1,40 +1,82 @@
-from io import StringIO
+from collections import defaultdict
+import sys
 
-fuck=StringIO("""examiwillpasstheexam
-5
-will
-pass
-the
-exam
-i
-""")
-def prefix_function(s):
-    # Функция возвращает массив длины |s|
-    n = len(s)
-    prefix_func = [None] * n
-    prefix_func[0] = 0
-    for i in range(1, n):
-        suffix_start_i = prefix_func[i - 1]
-        while suffix_start_i > 0 and s[suffix_start_i] != s[i]:
-            suffix_start_i = prefix_func[suffix_start_i - 1]
-        if s[suffix_start_i] == s[i]:
-            suffix_start_i += 1
-        prefix_func[i] = suffix_start_i
-    return prefix_func
-def find_all(s, p):
-    sep=chr(31)
-    pf=prefix_function(p+sep+s)
-    rs=[]
-    for ii, el in enumerate(pf):
-        i=ii-len(p)
-        if i<0:
-            continue
-        if el == len(p):
-            rs.append((i-len(p)+1, i+1))
-    return rs
-a=fuck.readline().strip()
-n=int(fuck.readline())
-ss=[]
-for iii in range(n):
-    ss.append(fuck.readline().strip())
-    print(find_all(a, ss[-1]))
+
+class PrefixTree:
+    def __init__(self):
+        self.terminals = set()
+        self.ribbles = defaultdict(dict)
+        self.root_id = 0
+        self.ribbles[self.root_id] = {}
+        self._free_id = self.root_id + 1
+
+    def _add_node(self, parent, symbol):
+        self.ribbles[parent] = self.ribbles.get(parent, {})
+        self.ribbles[parent][symbol] = self._free_id
+        self._free_id += 1
+        return self.ribbles[parent][symbol]
+
+    def get_node_by_symbol(self, parent, symbol):
+        return (
+            self.ribbles[parent][symbol]
+            if parent in self.ribbles and symbol in self.ribbles[parent]
+            else None
+        )
+
+    def is_root(self, node):
+        return node == self.root_id
+
+    def is_terminal(self, node):
+        return node in self.terminals
+
+    def add_string(self, string):
+        current_node = self.root_id
+        for i, symbol in enumerate(string):
+            symbol = string[i]  # На каждом шаге работаем с одним символом.
+
+            if self.get_node_by_symbol(current_node, symbol) is None:
+                self._add_node(current_node, symbol)
+            # Сдвинуться на следующий символ.
+            current_node = self.get_node_by_symbol(current_node, symbol)
+        self.terminals.add(current_node)
+        return current_node
+
+    def all_words_from(self, node):
+        res = []
+        f = []
+        st = [(node, [])]
+        while st:
+            node, dt = st.pop()
+            f.append(node)
+            if node in self.terminals:
+                res.append("".join(dt))
+            for symbol, next_id in self.ribbles[node].items():
+                st.append((next_id, dt + [symbol]))
+        return res
+
+
+def main():
+    with open("input.txt") as inp, open("output.txt", "w") as out:
+        a = inp.readline().strip()
+        n = int(inp.readline())
+        pt = PrefixTree()
+        for iii in range(n):
+            pt.add_string(inp.readline().strip())
+        can_split = [False for i in range(len(a))]
+        can_split.append(True)
+        for i in range(len(a) - 1, -1, -1):
+            ii = i
+            current = pt.root_id
+            while ii <= len(a):
+                if can_split[ii] and (pt.is_terminal(current) or pt.is_root(current)):
+                    can_split[i] = True
+                    break
+                if ii < len(a):
+                    next_node = pt.get_node_by_symbol(current, a[ii])
+                    if next_node is None:
+                        break
+                    else:
+                        current = next_node
+                ii = ii + 1
+
+        print("YES" if can_split[0] else "NO")
