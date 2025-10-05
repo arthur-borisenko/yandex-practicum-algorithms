6d314a8159f63533a27c477e4a64f2babfd561ce
Art <artour.borisenko@ya.ru>
2025-10-05 17:58:58 +0300

    ...

--- main/main/solution_n44_difference_in_trash_indices.py
+++ main/main/solution_n44_difference_in_trash_indices.py
@@ -1,18 +1,40 @@
-MAX_VALUE=1000000
+def count_less_or_equal_differences(elements, target):
+    a = 1
+    b = 0
+    d = 0
+    for i, el in enumerate(elements):
+        c = target + el
+        e = d - 1 if d > 0 else 0
+        a = max(a, i + 1)
+        while a < len(elements) and elements[a] <= c:
+            a += 1
+            e += 1
+        b += e
+        d = e
+    return b
+
+
+def solution(elements, k):
+    l, r = 0, elements[-1] - elements[0]
+    m = r // 2
+    while m >= 0:
+        m = (r + l) // 2
+        nm = count_less_or_equal_differences(elements, m)
+        if l == r:
+            return l
+        elif nm < k:
+            l = m + 1
+        elif nm >= k:
+            r = m
+
 
 def main():
-    """CPU - O(n log(k) + n log(n))
-    RAM - O(n)"""
-    with open("input.txt", "r") as inp, open("output.txt", "w") as outp:
-        n=int(inp.readline())
-        vals=sorted(map(int, inp.readline().split()))
-        k=int(inp.readline())
-        print(search(vals,k), file=outp)
+    with open("input.txt") as inp, open("output.txt", "w") as out:
+        n = int(inp.readline())
+        l = sorted(map(int, inp.readline().split()))
+        k = int(inp.readline())
+        print(solution(l, k), file=out)
 
-def search(v,k):
-    pass
-    #TODO: solve
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
