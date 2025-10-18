da47136cc38fb1e996d3065ba93ee23ebb3b2a06
Art <artour.borisenko@ya.ru>
2025-10-18 22:57:36 +0300

    fix codestyle

--- main/main/solution_n98_fibonacci_numbers_for_adults.py
+++ main/main/solution_n98_fibonacci_numbers_for_adults.py
@@ -1,7 +1,7 @@
 def main():
     with open("input.txt") as inp, open("output.txt", "w") as out:
         n = int(inp.readline())
-        m = 10 ** 9 + 7
+        m = 10**9 + 7
         prev_1 = 1
         prev_2 = 1
         current = 1
@@ -11,5 +11,6 @@ def main():
             prev_1 = current
         print(current, file=out)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
