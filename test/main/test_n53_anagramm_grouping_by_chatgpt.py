80af21dd57d721426ef3abab59801c6b051c2f9c
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:09:43 +0300

    solved task 53. (#56)
    * solved

---
+++ test/main/test_n53_anagramm_grouping_by_chatgpt.py
@@ -0,0 +1,37 @@
+import unittest
+
+import main.main.solution_n53_anagramm_grouping_by_chatgpt as solution
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+
+    def test_case1(self):
+        value = testUtil.file_test(
+            """6
+tan eat tea ate nat bat
+""",
+            solution.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """0 4
+1 2 3
+5
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """11
+5228 8225 @#$% #%$@ 8252 5282 $@#% %$@# agaf faga aafg
+""",
+            solution.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """0 1 4 5
+2 3 6 7
+8 9 10
+""".rstrip(),
+        )
