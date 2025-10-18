522dc3d0eb088ca0f1cd612f0fcdf32278036779
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:07:50 +0300

    task 44 (#49)
    * done

---
+++ test/main/test_n44_difference_in_trash_indices.py
@@ -0,0 +1,51 @@
+import unittest
+from main.main import solution_n44_difference_in_trash_indices as task
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """3
+2 3 4
+2
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """1
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """3
+1 3 1
+1
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """0
+""".rstrip(),
+        )
+
+    def test_case3(self):
+        value = testUtil.file_test(
+            """3
+1 3 5
+3
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """4
+""".rstrip(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
