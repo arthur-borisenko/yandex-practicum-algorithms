c0cd50a4224e49b4a542f808b9a12d3c2eaaf446
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:25:08 +0300

    task 95 solved (#98)
    * task 95 solved

---
+++ main/main/solution_n95_flower_field.py
@@ -0,0 +1,30 @@
+class DirEnum:
+    UP = "U"
+    RIGHT = "R"
+
+
+def main():
+    with open("input.txt") as inp, open("output.txt", "w") as out:
+        path = []
+        path2 = []
+        inp1 = []
+        n, m = map(int, inp.readline().split())
+        for i in range(n):
+            inp1.append(list(map(int, inp.readline().strip().rstrip())))
+        d = [[0 for _ in range(m)] for _ in range(n)]
+
+        for ii in range(-n + 1, 1):
+            i = -ii
+            for j in range(m):
+                d[i][j] = 0
+                if i + 1 < len(d):
+                    d[i][j] = max(d[i][j], d[i + 1][j])
+                if j > 0:
+                    d[i][j] = max(d[i][j], d[i][j - 1])
+                d[i][j] += inp1[i][j]
+                pass
+        print(d[0][-1], file=out)
+
+
+if __name__ == "__main__":
+    main()
