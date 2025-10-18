bd159cd09fd9db98ff234f10edb387de1ab0cf72
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:30:55 +0300

    task 115 solved (#118)
    * task 115 solved

---
+++ test/main/test_n115_search_with_shift.py
@@ -0,0 +1,43 @@
+import unittest
+import main.main.solution_n115_search_with_shift as task
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """9
+3 9 1 2 5 10 9 1 7
+2
+4 10
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """1 8
+
+""".rstrip(),
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """5
+1 2 3 4 5
+3
+10 11 12
+
+""".rstrip(),
+            task.main,
+        )
+        self.assertEqual(
+            value.rstrip(),
+            """1 2 3
+
+""".rstrip(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
