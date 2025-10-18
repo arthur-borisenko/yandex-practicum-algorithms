75a4066da549023b3d9eda2a276d8d00f490602a
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:22:26 +0300

    task 87 solved (#89)
    task 87 solved

---
+++ test/main/test_n87_bipartite_checking.py
@@ -0,0 +1,57 @@
+from main.main import solution_n87_bipartite_checking as task
+import unittest
+
+from test.utils import testUtil
+
+
+class TestCase(unittest.TestCase):
+    def test_case1(self):
+        value = testUtil.file_test(
+            """3 2
+1 2
+2 3
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """YES
+""",
+        )
+
+    def test_case2(self):
+        value = testUtil.file_test(
+            """3 3
+1 2
+2 3
+1 3
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """NO
+""",
+        )
+
+    def test_case3(self):
+        value = testUtil.file_test(
+            f"""6 6
+1 3
+2 4
+2 5
+3 4
+4 6
+5 6
+""",
+            task.main,
+        )
+        self.assertEqual(
+            value,
+            """YES
+""",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
