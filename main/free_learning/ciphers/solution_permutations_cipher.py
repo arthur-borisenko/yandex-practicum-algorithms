0109df080779e41c98b74c100a588b961c4b5c15
Art <artour.borisenko@ya.ru>
2025-10-18 22:26:08 +0300

    fix codestyle

--- main/free_learning/ciphers/solution_permutations_cipher.py
+++ main/free_learning/ciphers/solution_permutations_cipher.py
@@ -1,9 +1,71 @@
-A = {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л",
-     "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш",
-     "щ", "ъ", "ы", "ь", "э", "ю", "я", 'А', 'Б', 'В', 'Г', 'Д', 'Е',
-     'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
-     'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю',
-     'Я'}
+A = {
+    "а",
+    "б",
+    "в",
+    "г",
+    "д",
+    "е",
+    "ё",
+    "ж",
+    "з",
+    "и",
+    "й",
+    "к",
+    "л",
+    "м",
+    "н",
+    "о",
+    "п",
+    "р",
+    "с",
+    "т",
+    "у",
+    "ф",
+    "х",
+    "ц",
+    "ч",
+    "ш",
+    "щ",
+    "ъ",
+    "ы",
+    "ь",
+    "э",
+    "ю",
+    "я",
+    "А",
+    "Б",
+    "В",
+    "Г",
+    "Д",
+    "Е",
+    "Ё",
+    "Ж",
+    "З",
+    "И",
+    "Й",
+    "К",
+    "Л",
+    "М",
+    "Н",
+    "О",
+    "П",
+    "Р",
+    "С",
+    "Т",
+    "У",
+    "Ф",
+    "Х",
+    "Ц",
+    "Ч",
+    "Ш",
+    "Щ",
+    "Ъ",
+    "Ы",
+    "Ь",
+    "Э",
+    "Ю",
+    "Я",
+}
 
 
 def list_find(l, v):
@@ -26,8 +88,7 @@ class G:
         return r
 
 
-def preprocess_text(text, allowed_symbols,
-                    enable_auto_lowercase=True):
+def preprocess_text(text, allowed_symbols, enable_auto_lowercase=True):
     result = []
     for char in text:
         if char in allowed_symbols:
@@ -37,21 +98,17 @@ def preprocess_text(text, allowed_symbols,
     return result
 
 
-def encrypt(key: list, data: str, allowed_symbols=None,
-            enable_auto_lowercase=True):
+def encrypt(key: list, data: str, allowed_symbols=None, enable_auto_lowercase=True):
     if allowed_symbols is None:
         allowed_symbols = A
     block_length = len(key)
-    processed_text = preprocess_text(data, allowed_symbols,
-                                     enable_auto_lowercase)
+    processed_text = preprocess_text(data, allowed_symbols, enable_auto_lowercase)
     temp_result: list[list] = []
-    processed_text.extend(
-        [""] * (block_length - len(processed_text) % block_length))
+    processed_text.extend([""] * (block_length - len(processed_text) % block_length))
     for i in range(len(processed_text) // block_length):
         temp_result.append([""] * block_length)
         output_block = temp_result[i]
-        block = processed_text[
-                i * block_length:i * block_length + block_length]
+        block = processed_text[i * block_length : i * block_length + block_length]
         for j, el in enumerate(block):
             output_block[int(key[j]) - 1] = el
     result = []
@@ -61,13 +118,11 @@ def encrypt(key: list, data: str, allowed_symbols=None,
     return result
 
 
-def decrypt(key: list, data: str, allowed_symbols=None,
-            enable_auto_lowercase=True):
+def decrypt(key: list, data: str, allowed_symbols=None, enable_auto_lowercase=True):
     if allowed_symbols is None:
         allowed_symbols = A
     block_length = len(key)
-    processed_data = preprocess_text(data, allowed_symbols,
-                                     enable_auto_lowercase)
+    processed_data = preprocess_text(data, allowed_symbols, enable_auto_lowercase)
     large_columns_count = len(processed_data) % block_length
     processed_input = G(processed_data)
     temp_result = [[]] * block_length
@@ -76,13 +131,11 @@ def decrypt(key: list, data: str, allowed_symbols=None,
         if orig_column_pos < large_columns_count:
             temp_result[orig_column_pos] = []
             for _ in range(len(processed_data) // block_length + 1):
-                temp_result[orig_column_pos].append(
-                    next(processed_input))
+                temp_result[orig_column_pos].append(next(processed_input))
         else:
             temp_result[orig_column_pos] = []
             for _ in range(len(processed_data) // block_length):
-                temp_result[orig_column_pos].append(
-                    next(processed_input))
+                temp_result[orig_column_pos].append(next(processed_input))
     result = []
     for i in range(len(temp_result[0])):
         for j in range(len(temp_result)):
@@ -94,16 +147,16 @@ def decrypt(key: list, data: str, allowed_symbols=None,
 if __name__ == "__main__":
     mode = input("[E]ncrypt or [D]ecrypt? ")
     key = input(
-        "Enter key. split numbers using spaces (example: 1 2 3 4 5 6 7 8 9 10): ")
+        "Enter key. split numbers using spaces (example: 1 2 3 4 5 6 7 8 9 10): "
+    )
     text = input("Enter text: ")
     encrypt_inputs = {"E", "e", "encrypt", "Encrypt"}
     decrypt_inputs = {"D", "d", "decrypt", "Decrypt"}
     if mode in encrypt_inputs:
-        print("encrypted text: ",
-              *encrypt(list(map(int, key.split())), text), sep="")
+        print("encrypted text: ", *encrypt(list(map(int, key.split())), text), sep="")
     elif mode in decrypt_inputs:
-        print("decrypted text: ",
-              *decrypt(list(map(int, key.split())), text), sep="")
+        print("decrypted text: ", *decrypt(list(map(int, key.split())), text), sep="")
     else:
         raise ValueError(
-            "Invalid mode. Please enter either 'E' or 'D' to encrypt or decrypt.")
+            "Invalid mode. Please enter either 'E' or 'D' to encrypt or decrypt."
+        )
