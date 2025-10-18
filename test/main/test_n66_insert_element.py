16b25d10bbd2b72715745879b250c2cbfe1cec7a
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:14:48 +0300

    task 66 solved (#68)
    * task 66 solved

---
+++ test/main/test_n66_insert_element.py
@@ -0,0 +1,20 @@
+import unittest
+import main.main.solution_n66_insert_element as solution
+
+
+class Node:
+    def __init__(self, left=None, right=None, value=0):
+        self.right = right
+        self.left = left
+        self.value = value
+
+
+class TestCase(unittest.TestCase):
+
+    def test(self):
+        node1 = Node(None, None, 7)
+        node2 = Node(node1, None, 8)
+        node3 = Node(None, node2, 7)
+        new_head = solution.insert(node3, 6)
+        assert new_head is node3
+        assert new_head.left.value == 6
