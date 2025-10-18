7789061b424ccc8b278dbfb9cd6434e13a37a74f
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2025-10-19 00:12:48 +0300

    task 61 solved (#64)
    * task 61 solved

---
+++ test/main/test_n61_lightbulbs.py
@@ -0,0 +1,18 @@
+import main.main.solution_n61_lightbulbs as task
+import unittest
+
+
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.right = right
+        self.left = left
+
+
+class TestCase(unittest.TestCase):
+    def test(self):
+        node1 = Node(1)
+        node2 = Node(-5)
+        node3 = Node(3, node1, node2)
+        node4 = Node(2, node3, None)
+        assert task.solution(node4) == 3
